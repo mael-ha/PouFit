4462145fd0fe6a40bcc4e2bc92078ce8e0b82e55
Mael Harnois <mael.hrn@gmail.com>
2020-11-02 17:46:57 +0100

    update Meal model validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,5 @@
 class Meal < ApplicationRecord
   belongs_to :session
+  validates :name, presence: :true
+  validates :name, uniqueness: { scope: :session_id, message: "Une seule fois ce repas par jour!"}
 end
