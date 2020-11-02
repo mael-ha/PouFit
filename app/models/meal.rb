949afaec0872a9e77b82230dfd01467164096204
Mael Harnois <mael.hrn@gmail.com>
2020-11-02 17:50:47 +0100

    update Meal model validations - added MEAL_TYPE constant

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,6 @@ class Meal < ApplicationRecord
   belongs_to :session
   validates :name, presence: :true
   validates :name, uniqueness: { scope: :session_id, message: "Une seule fois ce repas par jour!"}
+  MEAL_TYPE = ["viande", "poisson", "végétarien", "végétalien"]
+  validates :type, inclusion: { in: MEAL_TYPE }
 end
