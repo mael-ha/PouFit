32cad309b375c2fb431fb2992d4582664c70e6be
Mael Harnois <mael.hrn@gmail.com>
2020-11-02 16:07:32 +0100

    create Health_data model

---
+++ db/migrate/20201102150718_create_health_data.rb
@@ -0,0 +1,14 @@
+class CreateHealthData < ActiveRecord::Migration[6.0]
+  def change
+    create_table :health_data do |t|
+      t.references :session, null: false, foreign_key: true
+      t.integer :body_weight_main
+      t.integer :body_weight_second
+      t.real :body_weight
+      t.integer :muscular_mass
+      t.integer :fat_mass
+
+      t.timestamps
+    end
+  end
+end
