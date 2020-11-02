09a296a6fa747e6f9b4f3797048fad454ca1697d
Mael Harnois <mael.hrn@gmail.com>
2020-11-02 18:34:56 +0100

    commit after rails db:migrate

--- db/migrate/20201102145654_create_ref_exercices.rb
+++ db/migrate/20201102145654_create_ref_exercices.rb
@@ -1,16 +1,16 @@
 class CreateRefExercices < ActiveRecord::Migration[6.0]
   def change
     create_table :ref_exercices do |t|
-      t.id :name
+      t.string :name
       t.references :base, null: false, foreign_key: true
       t.string :type
       t.integer :timer
       t.integer :number_of_reps
-      t.integer :weight_value, default: 0
+      t.integer :weight_value
       t.integer :duration
       t.text :comment
       t.references :ref_workout, null: false, foreign_key: true
-      t.integer :time_delay_next, default: 0
+      t.integer :time_delay_next
       t.integer :index_in_workout
       t.boolean :belongs_to_block
       t.integer :index_in_block
--- db/migrate/20201102153645_create_exercices.rb
+++ db/migrate/20201102153645_create_exercices.rb
@@ -6,11 +6,11 @@ class CreateExercices < ActiveRecord::Migration[6.0]
       t.string :type
       t.integer :timer
       t.integer :number_of_reps
-      t.integer :weight_value, default: 0
+      t.integer :weight_value
       t.integer :duration
       t.text :comment
       t.references :workout, null: false, foreign_key: true
-      t.integer :time_delay_next, default: 0
+      t.integer :time_delay_next
       t.integer :index_in_workout
       t.boolean :belongs_to_block
       t.integer :index_in_block
