1cd7895252882cbdf2aaef4b944590c435a7b5b8
Mael Harnois <mael.hrn@gmail.com>
2020-11-04 13:36:28 +0100

    added seeds for Ref_block

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,6 +42,7 @@ p2 = Base.create!(name: "Pompes larges", type: "Effort", muscular_group: "pecs")
 t1 = Base.create!(name: "Tractions Supination", type: "Effort", muscular_group: "dos")
 t2 = Base.create!(name: "Tractions Supination", type: "Effort", muscular_group: "dos")
 
+
 puts "   => Created exercice bases: #{Base.all.each { |base| puts "#{base.name}" }}"
 puts "... Creating Ref_exercices"
 
@@ -182,5 +183,41 @@ i = 1
 end
 puts "... => 6x5t2 created"
 
-puts "... Creating Ref_blocks"
+puts "... Creating 2 Ref_blocks : Poitrine and Dos"
+block_poitrine = Block.create!(
+  name: "Block Poitrine",
+  ref_workout_id: nil,
+  index_in_workout: nil,
+  time_delay_next: 180000,
+  user: sam
+  )
+block_dos = Block.create!(
+  name: "Block Dos",
+  ref_workout_id: nil,
+  index_in_workout: nil,
+  time_delay_next: 0,
+  user: sam
+  )
 
+exo_poitrine = [6x5dips, 6x5p1, 6x5p2]
+puts "... Adding  6x5dips, 6x5p1 and 6x5p2 to Ref_block Poitrine"
+exo_poitrine.each do |mini_block|
+  i = 1
+  mini_block.each do |exo|
+    exo.ref_block = block_poitrine
+    index_in_block = i
+    i += 1
+  end
+end
+
+puts "... Adding  6x5t1 and 6x5t2to Ref_block Dos"
+exo_dos = [6x5t1, 6x5t2]
+exo_dos.each do |mini_block|
+  i = 1
+  mini_block.each do |exo|
+    exo.ref_block = block_dos
+    index_in_block = i
+    i += 1
+  end
+end
+puts "   => Created 2 blocks : Poitrine and Dos"
