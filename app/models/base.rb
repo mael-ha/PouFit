4b14027cdd1e9505ccffabc45588d2162893502d
Mael Harnois <mael.hrn@gmail.com>
2020-11-04 13:20:33 +0100

    Created seeds for User, Base, Ref_exercice

--- app/models/base.rb
+++ app/models/base.rb
@@ -1,7 +1,7 @@
 class Base < ApplicationRecord
   validates :name, :type, :muscular_group, presence: :true
   validates :name, :uniqueness :true
-  BASE_TYPE = ["Échauffement, Effort, Étirement"]
+  BASE_TYPE = ["Effort, Étirement"]
   validates :type, inclusion: { in: BASE_TYPE }
   MUSCULAR_GROUP = ["bras", "pecs", "abdos", "dos", "jambes", "mix haut", "mix bas", "full-body"]
   validates :muscular_group, inclusion: { in: MUSCULAR_GROUP }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,182 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "... Starting seeds"
+puts "... Erase all database"
+Ref_workout.destroy_all
+Ref_block.destroy_all
+Ref_exercise.destroy_all
+Workout.destroy_all
+Block.destroy_all
+Exercise.destroy_all
+Base.destroy_all
+Health_data.destroy_all
+Meal.destroy_all
+Day_session.destroy_all
+User.destroy_all
+
+
+# Create User
+puts "... Creating 1 user"
+sam = User.create!(
+  first_name: "Sam",
+  last_name: "Sam",
+  username: "Samsam",
+  birthdate: Date.new(1994,5,15),
+  gender: "Femme",
+  email: "samsam@poufit.com",
+  password: "poufit"
+  )
+puts "   => Created User ##{sam.id} - username: #{sam.username}"
+
+puts "... Creating 6 exercice bases"
+
+jj = Base.create!(name: "Jumping Jack", type: "Effort", muscular_group: "full-body")
+dips = Base.create!(name: "Dips", type: "Effort", muscular_group: "mix haut")
+p1 = Base.create!(name: "Pompes serrées", type: "Effort", muscular_group: "pecs")
+p2 = Base.create!(name: "Pompes larges", type: "Effort", muscular_group: "pecs")
+t1 = Base.create!(name: "Tractions Supination", type: "Effort", muscular_group: "dos")
+t2 = Base.create!(name: "Tractions Supination", type: "Effort", muscular_group: "dos")
+
+puts "   => Created exercice bases: #{Base.all.each { |base| puts "#{base.name}" }}"
+puts "... Creating Ref_exercices"
+
+puts "... // Creating 90s Jumping Jack"
+  90sjj = Ref_exercice.create!(
+    name: "5 #{jj.name} - #{i}/6",
+    base: jj,
+    type: "timer",
+    timer: 90000,
+    number_of_reps: 6,
+    weight_value: 0,
+    comment: "#{jj.name} crossfit",
+    time_delay_next: 120000,
+    user: sam,
+    index_in_block: nil,
+    index_in_workout: nil,
+    ref_block_id: nil,
+    ref_workout_id: nil
+puts "... => 90s jumping jack created"
+
+puts "... // Creating 6x5dips"
+i = 1
+6x5dips = []
+6.times do
+  5dips = Ref_exercice.create!(
+    name: "5 #{dips.name} - #{i}/6",
+    base: dips,
+    type: "reps",
+    timer: nil,
+    number_of_reps: 6,
+    weight_value: 0,
+    comment: "#{dips.name} full amplitude",
+    time_delay_next: 25000,
+    user: sam,
+    index_in_block: nil,
+    index_in_workout: nil,
+    ref_block_id: nil,
+    ref_workout_id: nil
+    )
+  6x5dips << 5dips
+  i += 1
+end
+puts "... => 6x5dips created"
+
+puts "... // Creating 6x5p1"
+i = 1
+6x5p1 = []
+6.times do
+  5p1 = Ref_exercice.create!(
+    name: "5 #{p1.name} - #{i}/6",
+    base: p1,
+    type: "reps",
+    timer: nil,
+    number_of_reps: 6,
+    weight_value: 0,
+    comment: "#{p1} full amplitude, au sol, non surélevé",
+    time_delay_next: 25000,
+    user: sam,
+    index_in_block: nil,
+    index_in_workout: nil,
+    ref_block_id: nil,
+    ref_workout_id: nil
+    )
+  6x5p1 << 5p1
+  i += 1
+end
+puts "... => 6x5p1 created"
+
+puts "... // Creating 6x5p2"
+i = 1
+6x5p2 = []
+6.times do
+  5p2 = Ref_exercice.create!(
+    name: "5 #{p2.name} - #{i}/6",
+    base: p2,
+    type: "reps",
+    timer: nil,
+    number_of_reps: 6,
+    weight_value: 0,
+    comment: "#{p2} full amplitude, au sol, non surélevé",
+    time_delay_next: 25000,
+    user: sam,
+    index_in_block: nil,
+    index_in_workout: nil,
+    ref_block_id: nil,
+    ref_workout_id: nil
+    )
+  6x5p2 << 5p2
+  i += 1
+end
+puts "... => 6x5p2 created"
+
+puts "... // Creating 6x5t1"
+i = 1
+6x5t1 = []
+6.times do
+  5t1 = Ref_exercice.create!(
+    name: "5 #{t1.name} - #{i}/6",
+    base: t1,
+    type: "reps",
+    timer: nil,
+    number_of_reps: 6,
+    weight_value: 0,
+    comment: "#{t1} full amplitude, au sol, non surélevé",
+    time_delay_next: 25000,
+    user: sam,
+    index_in_block: nil,
+    index_in_workout: nil,
+    ref_block_id: nil,
+    ref_workout_id: nil
+    )
+  6x5t1 << 5t1
+  i += 1
+end
+puts "... => 6x5t1 created"
+
+puts "... // Creating 6x5t2"
+i = 1
+6x5t2 = []
+6.times do
+  5t2 = Ref_exercice.create!(
+    name: "5 #{t2.name} - #{i}/6",
+    base: t2,
+    type: "reps",
+    timer: nil,
+    number_of_reps: 6,
+    weight_value: 0,
+    comment: "#{t2} full amplitude, au sol, non surélevé",
+    time_delay_next: 25000,
+    user: sam,
+    index_in_block: nil,
+    index_in_workout: nil,
+    ref_block_id: nil,
+    ref_workout_id: nil
+    )
+  6x5t2 << 5t2
+  i += 1
+end
+puts "... => 6x5t2 created"
+
+puts "... Creating Ref_blocks"
+
